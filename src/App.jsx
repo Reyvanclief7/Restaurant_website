cac35b9e958a4b5778cacba00790ded90492f15d
ClieffordNOya <clieffordnoya28@gmail.com>
2024-11-15 15:05:25 +0800

    add service page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./components/Home/Home"; // Mengimpor Home dari folder components
 import About from "./components/About/About"; // Mengimpor About dari folder components
+import Service from "./components/Service/Service";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/About" element={<About />} />
+      <Route exact path="/About" element={<Service />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Service/Service.js
@@ -0,0 +1,235 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Service = () => {
+    return (
+<div className="container-xxl bg-white p-0">
+  {/* Navbar & Hero Start */}
+  <div className="container-xxl position-relative p-0">
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
+          <Link to="/" className="navbar-brand p-0">
+            <h1 className="text-primary m-0">
+              <i className="fa fa-utensils me-3" />
+              Restoran
+            </h1>
+            {/* Optional Logo Image */}
+            {/* <img src="img/logo.png" alt="Logo" /> */}
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+          >
+            <span className="fa fa-bars" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <div className="navbar-nav ms-auto py-0 pe-4">
+              <Link to="/" className="nav-item nav-link active">
+                Home
+              </Link>
+              <Link to="/About" className="nav-item nav-link">
+                About
+              </Link>
+              <Link to="/service" className="nav-item nav-link">
+                Service
+              </Link>
+              <Link to="/menu" className="nav-item nav-link">
+                Menu
+              </Link>
+              <div className="nav-item dropdown">
+                <a
+                  href="#"
+                  className="nav-link dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                >
+                  Pages
+                </a>
+                <div className="dropdown-menu m-0">
+                  <Link to="/booking" className="dropdown-item">
+                    Booking
+                  </Link>
+                  <Link to="/team" className="dropdown-item">
+                    Our Team
+                  </Link>
+                  <Link to="/testimonial" className="dropdown-item">
+                    Testimonial
+                  </Link>
+                </div>
+              </div>
+              <Link to="/contact" className="nav-item nav-link">
+                Contact
+              </Link>
+            </div>
+            <Link to="/book-table" className="btn btn-primary py-2 px-4">
+              Book A Table
+            </Link>
+          </div>
+        </nav>
+    <div className="container-xxl py-5 bg-dark hero-header mb-5">
+      <div className="container text-center my-5 pt-5 pb-4">
+        <h1 className="display-3 text-white mb-3 animated slideInDown">Services</h1>
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb justify-content-center text-uppercase">
+            <li className="breadcrumb-item"><a href="#">Home</a></li>
+            <li className="breadcrumb-item"><a href="#">Pages</a></li>
+            <li className="breadcrumb-item text-white active" aria-current="page">Service</li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+  </div>
+  {/* Navbar & Hero End */}
+  {/* Service Start */}
+  <div className="container-xxl py-5">
+    <div className="container">
+      <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
+        <h5 className="section-title ff-secondary text-center text-primary fw-normal">Our Services</h5>
+        <h1 className="mb-5">Explore Our Services</h1>
+      </div>
+      <div className="row g-4">
+        <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+          <div className="service-item rounded pt-3">
+            <div className="p-4">
+              <i className="fa fa-3x fa-user-tie text-primary mb-4" />
+              <h5>Master Chefs</h5>
+              <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+          <div className="service-item rounded pt-3">
+            <div className="p-4">
+              <i className="fa fa-3x fa-utensils text-primary mb-4" />
+              <h5>Quality Food</h5>
+              <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+          <div className="service-item rounded pt-3">
+            <div className="p-4">
+              <i className="fa fa-3x fa-cart-plus text-primary mb-4" />
+              <h5>Online Order</h5>
+              <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+          <div className="service-item rounded pt-3">
+            <div className="p-4">
+              <i className="fa fa-3x fa-headset text-primary mb-4" />
+              <h5>24/7 Service</h5>
+              <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+          <div className="service-item rounded pt-3">
+            <div className="p-4">
+              <i className="fa fa-3x fa-user-tie text-primary mb-4" />
+              <h5>Master Chefs</h5>
+              <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+          <div className="service-item rounded pt-3">
+            <div className="p-4">
+              <i className="fa fa-3x fa-utensils text-primary mb-4" />
+              <h5>Quality Food</h5>
+              <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+          <div className="service-item rounded pt-3">
+            <div className="p-4">
+              <i className="fa fa-3x fa-cart-plus text-primary mb-4" />
+              <h5>Online Order</h5>
+              <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+          <div className="service-item rounded pt-3">
+            <div className="p-4">
+              <i className="fa fa-3x fa-headset text-primary mb-4" />
+              <h5>24/7 Service</h5>
+              <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  {/* Service End */}
+  {/* Footer Start */}
+  <div className="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+    <div className="container py-5">
+      <div className="row g-5">
+        <div className="col-lg-3 col-md-6">
+          <h4 className="section-title ff-secondary text-start text-primary fw-normal mb-4">Company</h4>
+          <a className="btn btn-link" href>About Us</a>
+          <a className="btn btn-link" href>Contact Us</a>
+          <a className="btn btn-link" href>Reservation</a>
+          <a className="btn btn-link" href>Privacy Policy</a>
+          <a className="btn btn-link" href>Terms &amp; Condition</a>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <h4 className="section-title ff-secondary text-start text-primary fw-normal mb-4">Contact</h4>
+          <p className="mb-2"><i className="fa fa-map-marker-alt me-3" />123 Street, New York, USA</p>
+          <p className="mb-2"><i className="fa fa-phone-alt me-3" />+012 345 67890</p>
+          <p className="mb-2"><i className="fa fa-envelope me-3" />info@example.com</p>
+          <div className="d-flex pt-2">
+            <a className="btn btn-outline-light btn-social" href><i className="fab fa-twitter" /></a>
+            <a className="btn btn-outline-light btn-social" href><i className="fab fa-facebook-f" /></a>
+            <a className="btn btn-outline-light btn-social" href><i className="fab fa-youtube" /></a>
+            <a className="btn btn-outline-light btn-social" href><i className="fab fa-linkedin-in" /></a>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <h4 className="section-title ff-secondary text-start text-primary fw-normal mb-4">Opening</h4>
+          <h5 className="text-light fw-normal">Monday - Saturday</h5>
+          <p>09AM - 09PM</p>
+          <h5 className="text-light fw-normal">Sunday</h5>
+          <p>10AM - 08PM</p>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <h4 className="section-title ff-secondary text-start text-primary fw-normal mb-4">Newsletter</h4>
+          <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+          <div className="position-relative mx-auto" style={{maxWidth: 400}}>
+            <input className="form-control border-primary w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email" />
+            <button type="button" className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="container">
+      <div className="copyright">
+        <div className="row">
+          <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+            © <a className="border-bottom" href="#">Your Site Name</a>, All Right Reserved. 
+            {/*/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. *** /*/}
+            Designed By <a className="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+          </div>
+          <div className="col-md-6 text-center text-md-end">
+            <div className="footer-menu">
+              <a href>Home</a>
+              <a href>Cookies</a>
+              <a href>Help</a>
+              <a href>FQAs</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  {/* Footer End */}
+  {/* Back to Top */}
+  <a href="#" className="btn btn-lg btn-primary btn-lg-square back-to-top"><i className="bi bi-arrow-up" /></a>
+</div>
+
+    )
+}
+export default Service
