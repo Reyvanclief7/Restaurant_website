007bf870b2ff4ace4dfe79196ed22fbe6c30a22b
ClieffordNOya <clieffordnoya28@gmail.com>
2024-11-15 15:20:01 +0800

    add menu page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import React from "react";
 import Home from "./components/Home/Home"; // Mengimpor Home dari folder components
 import About from "./components/About/About"; // Mengimpor About dari folder components
 import Service from "./components/Service/Service";
+import Menu from "./components/Menu/Menu";  
 
 function App() {
   return (
     <Router>
       <Routes>
       <Route exact path="/" element={<Home />} />
-      <Route exact path="/About" element={<About />} />
       <Route exact path="/About" element={<Service />} />
+      <Route exact path="/About" element={<About />} />
+      <Route exact path="/About" element={<Menu />} />
       </Routes>
     </Router>
   );
