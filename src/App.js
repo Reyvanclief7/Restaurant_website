b35391628c17149ea1727dd1f7681d8e12212c6e
ClieffordNOya <clieffordnoya28@gmail.com>
2024-11-17 21:32:30 +0800

    adding Components Team

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import About from "./Components/About";
 import Menu from "./Components/Menu";
 import NavHero from "./Components/navbar&hero";
-import Reservation1 from "./Components/Reservation1";
-import Reservation2 from "./Components/Reservation2";
 import Service from "./Components/Service";
+import Team from "./Components/Team";
+
 
 const App = () => {
   return (
@@ -21,114 +21,146 @@ const App = () => {
        <Menu />
       {/* Menu End */}
       {/* Reservation Start */}
-       <Reservation1 />
-       <Reservation2 />
-      {/* Reservation Start */}
-      {/* Team Start */}
-      <div className="container-xxl pt-5 pb-3">
-        <div className="container">
-          <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-            <h5 className="section-title ff-secondary text-center text-primary fw-normal">
-              Team Members
-            </h5>
-            <h1 className="mb-5">Our Master Chefs</h1>
+      <div
+        className="container-xxl py-5 px-0 wow fadeInUp"
+        data-wow-delay="0.1s"
+      >
+        <div className="row g-0">
+          <div className="col-md-6">
+            <div className="video">
+              <button
+                type="button"
+                className="btn-play"
+                data-bs-toggle="modal"
+                data-src="https://www.youtube.com/embed/DWRcNpR6Kdc"
+                data-bs-target="#videoModal"
+              >
+                <span />
+              </button>
+            </div>
           </div>
-          <div className="row g-4">
-            <div
-              className="col-lg-3 col-md-6 wow fadeInUp"
-              data-wow-delay="0.1s"
-            >
-              <div className="team-item text-center rounded overflow-hidden">
-                <div className="rounded-circle overflow-hidden m-4">
-                  <img className="img-fluid" src="img/team-1.jpg" alt />
+          <div className="col-md-6 bg-dark d-flex align-items-center">
+            <div className="p-5 wow fadeInUp" data-wow-delay="0.2s">
+              <h5 className="section-title ff-secondary text-start text-primary fw-normal">
+                Reservation
+              </h5>
+              <h1 className="text-white mb-4">Book A Table Online</h1>
+              <form>
+                <div className="row g-3">
+                  <div className="col-md-6">
+                    <div className="form-floating">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        placeholder="Your Name"
+                      />
+                      <label htmlFor="name">Your Name</label>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-floating">
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        placeholder="Your Email"
+                      />
+                      <label htmlFor="email">Your Email</label>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div
+                      className="form-floating date"
+                      id="date3"
+                      data-target-input="nearest"
+                    >
+                      <input
+                        type="text"
+                        className="form-control datetimepicker-input"
+                        id="datetime"
+                        placeholder="Date & Time"
+                        data-target="#date3"
+                        data-toggle="datetimepicker"
+                      />
+                      <label htmlFor="datetime">Date &amp; Time</label>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-floating">
+                      <select className="form-select" id="select1">
+                        <option value={1}>People 1</option>
+                        <option value={2}>People 2</option>
+                        <option value={3}>People 3</option>
+                      </select>
+                      <label htmlFor="select1">No Of People</label>
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <div className="form-floating">
+                      <textarea
+                        className="form-control"
+                        placeholder="Special Request"
+                        id="message"
+                        style={{ height: 100 }}
+                        defaultValue={""}
+                      />
+                      <label htmlFor="message">Special Request</label>
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <button
+                      className="btn btn-primary w-100 py-3"
+                      type="submit"
+                    >
+                      Book Now
+                    </button>
+                  </div>
                 </div>
-                <h5 className="mb-0">Full Name</h5>
-                <small>Designation</small>
-                <div className="d-flex justify-content-center mt-3">
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-facebook-f" />
-                  </a>
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-twitter" />
-                  </a>
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-instagram" />
-                  </a>
-                </div>
-              </div>
+              </form>
             </div>
-            <div
-              className="col-lg-3 col-md-6 wow fadeInUp"
-              data-wow-delay="0.3s"
-            >
-              <div className="team-item text-center rounded overflow-hidden">
-                <div className="rounded-circle overflow-hidden m-4">
-                  <img className="img-fluid" src="img/team-2.jpg" alt />
-                </div>
-                <h5 className="mb-0">Full Name</h5>
-                <small>Designation</small>
-                <div className="d-flex justify-content-center mt-3">
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-facebook-f" />
-                  </a>
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-twitter" />
-                  </a>
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-instagram" />
-                  </a>
-                </div>
-              </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="modal fade"
+        id="videoModal"
+        tabIndex={-1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content rounded-0">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Youtube Video
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
             </div>
-            <div
-              className="col-lg-3 col-md-6 wow fadeInUp"
-              data-wow-delay="0.5s"
-            >
-              <div className="team-item text-center rounded overflow-hidden">
-                <div className="rounded-circle overflow-hidden m-4">
-                  <img className="img-fluid" src="img/team-3.jpg" alt />
-                </div>
-                <h5 className="mb-0">Full Name</h5>
-                <small>Designation</small>
-                <div className="d-flex justify-content-center mt-3">
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-facebook-f" />
-                  </a>
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-twitter" />
-                  </a>
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-instagram" />
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div
-              className="col-lg-3 col-md-6 wow fadeInUp"
-              data-wow-delay="0.7s"
-            >
-              <div className="team-item text-center rounded overflow-hidden">
-                <div className="rounded-circle overflow-hidden m-4">
-                  <img className="img-fluid" src="img/team-4.jpg" alt />
-                </div>
-                <h5 className="mb-0">Full Name</h5>
-                <small>Designation</small>
-                <div className="d-flex justify-content-center mt-3">
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-facebook-f" />
-                  </a>
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-twitter" />
-                  </a>
-                  <a className="btn btn-square btn-primary mx-1" href>
-                    <i className="fab fa-instagram" />
-                  </a>
-                </div>
+            <div className="modal-body">
+              {/* 16:9 aspect ratio */}
+              <div className="ratio ratio-16x9">
+                <iframe
+                  className="embed-responsive-item"
+                  src
+                  id="video"
+                  allowFullScreen
+                  allowscriptaccess="always"
+                  allow="autoplay"
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
+      {/* Reservation Start */}
+      {/* Team Start */}
+       <Team />
       {/* Team End */}
       {/* Testimonial Start */}
       <div className="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
