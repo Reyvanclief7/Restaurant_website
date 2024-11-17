237f790ab87f7d29e1855213e88689d02d032fde
Reyvan clief <rey17van17@gmail.com>
2024-11-18 00:02:51 +0800

    convert navbar & hero to firebase

--- src/Components/navbar&hero/index.js
+++ src/Components/navbar&hero/index.js
@@ -1,5 +1,5 @@
 import { getDatabase, ref, onValue } from "firebase/database";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const NavHero = () => {
   const [navHero, setNavHero] = useState([]);
@@ -9,98 +9,76 @@ const NavHero = () => {
     onValue(navHeroRef, (snapshot) => {
       const data = snapshot.val();
       setNavHero(data);
-    });
-  }, []);
-    return (
-        <div className="container-xxl position-relative p-0">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
-          <a href className="navbar-brand p-0">
-            <h1 className="text-primary m-0">
-              <i className="fa fa-utensils me-3" />
-              {navHero.title}
-            </h1>
-            {/* <img src="img/logo.png" alt="Logo"> */}
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarCollapse"
-          >
-            <span className="fa fa-bars" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarCollapse">
-            <div className="navbar-nav ms-auto py-0 pe-4">
-              <a href="index.html" className="nav-item nav-link active">
-                Home
-              </a>
-              <a href="about.html" className="nav-item nav-link">
-                About
-              </a>
-              <a href="service.html" className="nav-item nav-link">
-                Service
-              </a>
-              <a href="menu.html" className="nav-item nav-link">
-                Menu
-              </a>
-              <div className="nav-item dropdown">
-                <a
-                  href="#"
-                  className="nav-link dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                >
-                  Pages
-                </a>
-                <div className="dropdown-menu m-0">
-                  <a href="booking.html" className="dropdown-item">
-                    Booking
-                  </a>
-                  <a href="team.html" className="dropdown-item">
-                    Our Team
-                  </a>
-                  <a href="testimonial.html" className="dropdown-item">
-                    Testimonial
-                  </a>
-                </div>
-              </div>
-              <a href="contact.html" className="nav-item nav-link">
-                Contact
-              </a>
-            </div>
-            <a href className="btn btn-primary py-2 px-4">
-              Book A Table
+    });
+  }, []);
+  return (
+    <div className="container-xxl position-relative p-0">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
+        <a href className="navbar-brand p-0">
+          <h1 className="text-primary m-0">
+            <i className="fa fa-utensils me-3" />
+            {navHero.title}
+          </h1>
+          {/* <img src="img/logo.png" alt="Logo"> */}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+        >
+          <span className="fa fa-bars" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <div className="navbar-nav ms-auto py-0 pe-4">
+            <a href="index.html" className="nav-item nav-link active">
+              {navHero.home}
+            </a>
+            <a href="about.html" className="nav-item nav-link">
+              {navHero.about}
+            </a>
+            <a href="service.html" className="nav-item nav-link">
+              {navHero.service}
+            </a>
+            <a href="menu.html" className="nav-item nav-link">
+              {navHero.menu}
+            </a>
+            <a href="contact.html" className="nav-item nav-link">
+              {navHero.contact}
             </a>
           </div>
-        </nav>
-        <div className="container-xxl py-5 bg-dark hero-header mb-5">
-          <div className="container my-5 py-5">
-            <div className="row align-items-center g-5">
-              <div className="col-lg-6 text-center text-lg-start">
-                <h1 className="display-3 text-white animated slideInLeft">
-                  Enjoy Our
-                  <br />
-                  Delicious Meal
-                </h1>
-                <p className="text-white animated slideInLeft mb-4 pb-2">
-                  Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit.
-                  Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,
-                  sed stet lorem sit clita duo justo magna dolore erat amet
-                </p>
-                <a
-                  href
-                  className="btn btn-primary py-sm-3 px-sm-5 me-3 animated slideInLeft"
-                >
-                  Book A Table
-                </a>
-              </div>
-              <div className="col-lg-6 text-center text-lg-end overflow-hidden">
-                <img className="img-fluid" src="img/hero.png" alt />
-              </div>
+          <a href className="btn btn-primary py-2 px-4">
+            {navHero.booking}
+          </a>
+        </div>
+      </nav>
+      <div className="container-xxl py-5 bg-dark hero-header mb-5">
+        <div className="container my-5 py-5">
+          <div className="row align-items-center g-5">
+            <div className="col-lg-6 text-center text-lg-start">
+              <h1 className="display-3 text-white animated slideInLeft">
+                {navHero.text1}
+                <br />
+                {navHero.text2}
+              </h1>
+              <p className="text-white animated slideInLeft mb-4 pb-2">
+                {navHero.text3}
+              </p>
+              <a
+                href
+                className="btn btn-primary py-sm-3 px-sm-5 me-3 animated slideInLeft"
+              >
+                {navHero.booking}
+              </a>
+            </div>
+            <div className="col-lg-6 text-center text-lg-end overflow-hidden">
+              <img className="img-fluid" src="img/hero.png" alt />
             </div>
           </div>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default NavHero
+export default NavHero;
